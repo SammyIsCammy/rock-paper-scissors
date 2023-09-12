90a10e50e3814a1582b9a82565be882e6804166f
Samuel Camacho <mr.sammycamacho@gmail.com>
2023-09-12 18:19:22 -0400

    Added basic game functionality. Game is currently console based only

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
+let choices = ['Rock', 'Paper', 'Scissors'];
+
 // Creating a function that randomly returns Rock, Paper, Scissors. This will act as the computer's choice against the user
 function getComputerChoice() {
-    let computerChoices = ['Rock', 'Paper', 'Scissors'];
-    let computerChoice = Math.floor(Math.random() * computerChoices.length);
-    return computerChoices[computerChoice];
+    let computerChoice = Math.floor(Math.random() * choices.length);
+    return choices[computerChoice];
 }
 
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection){
+        return "It's a tie!";
+    } else if (playerSelection === choices[0] && computerSelection === choices[1]) {
+        return "Computer wins!";
+    } else if (playerSelection === choices[1] && computerSelection === choices[0]) {
+        return "You win!";
+    } else if (playerSelection === choices[1] && computerSelection === choices[2]) {
+        return "Computer wins!";
+    } else if (playerSelection === choices[2] && computerSelection === choices[1]) {
+        return "You win!";
+    } else if (playerSelection === choices[0] && computerSelection === choices[2]) {
+        return "You win!";
+    } else if (playerSelection === choices[2] && computerSelection === choices[0]) {
+        return "Computer wins!";
+    }
+};
 
+console.log(playRound(getComputerChoice(), getComputerChoice()));
